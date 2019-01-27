da9fedb04080b710c16b1f72e6e97d54cbb97f5e
Mazen Refaat <m.ref3at92@gmail.com>
2019-01-28 01:20:32 +0200

    IE Support

--- webpack.config.js
+++ webpack.config.js
@@ -7,6 +7,9 @@ module.exports = {
         path: path.join(__dirname, '/dist'),
         filename: 'app_bundle.js'
     },
+    devServer: {
+        disableHostCheck: true
+    },
     module: {
         rules: [
             {
